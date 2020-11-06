8097a0454b573dcc123ded57bfdad5553c38e988
pratap_narayan <pratap_narayan@apple.com>
2020-11-06 15:39:48 +0530

    partitioningBy feature

--- modern-java8/src/main/java/com/pratap/java8/stream/terminal/StreamGroupingByExample.java
+++ modern-java8/src/main/java/com/pratap/java8/stream/terminal/StreamGroupingByExample.java
@@ -2,6 +2,7 @@ package com.pratap.java8.stream.terminal;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import com.pratap.java8.beans.Student;
@@ -9,6 +10,11 @@ import com.pratap.java8.data.StudentDataBase;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
 import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.minBy;
+
+import java.util.Comparator;
 
 import java.util.LinkedHashMap;
 
@@ -54,6 +60,40 @@ public class StreamGroupingByExample {
 		
 		System.out.println(studentLinkedHM);
 	}
+	
+	// use case - 4 calculate top gpa students in each grades and least gpa students in each grade.
+	
+	public static void calculateTopGpaOptional() {
+		Map<Integer, Optional<Student>> studentOptional = students.stream()
+					.collect(groupingBy(Student::getGradeLevel, 
+									maxBy(Comparator.comparing(Student::getGpa))));
+		
+		System.out.println(studentOptional);
+	}
+	
+	public static void calculateTopGpaStudent() {
+		Map<Integer, Student> topGpaStudent = students.stream()
+					.collect(groupingBy(Student::getGradeLevel, 
+									collectingAndThen(maxBy(Comparator.comparing(Student::getGpa)), Optional::get)));
+		
+		System.out.println(topGpaStudent);
+	}
+	
+	public static void calculateMinGpaOptional() {
+		Map<Integer, Optional<Student>> studentOptional = students.stream()
+					.collect(groupingBy(Student::getGradeLevel, 
+									minBy(Comparator.comparing(Student::getGpa))));
+		
+		System.out.println(studentOptional);
+	}
+	
+	public static void calculateMinGpaStudent() {
+		Map<Integer, Student> topGpaStudent = students.stream()
+					.collect(groupingBy(Student::getGradeLevel, 
+									collectingAndThen(minBy(Comparator.comparing(Student::getGpa)), Optional::get)));
+		
+		System.out.println(topGpaStudent);
+	}
 	public static void main(String[] args) {
 
 		System.out.println(groupStudentsByGender());
@@ -63,6 +103,10 @@ public class StreamGroupingByExample {
 		twoLevelGroupingBy_1();
 		twoLevelGroupingBy_2();
 		threeArgumentGroupBy();
+		calculateTopGpaOptional();
+		calculateTopGpaStudent();
+		calculateMinGpaOptional();
+		calculateMinGpaStudent();
 	}
 
 }
